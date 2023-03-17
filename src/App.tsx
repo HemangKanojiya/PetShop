83374371717354d645159c787956ed1acba7b2cc
HemangKanojiya <Hemang.Kanojiya@technomark.io>
2023-03-17 14:52:09 +0530

    About page Message Compnent

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import Nutritions from './Components/Nutrition/Nutritions';
 import Categories from './Components/Top Category/Categories'
+import Message from './Components/AboutUs/Message From Funder/Message';
 
 function App() {
   return (
-    <Categories />
+    <Message />
   );
 }
 
